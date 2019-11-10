309838bc4a0e880dfb1b84f6b25f7fdd19c4b7be
Peter Green <p.l.green@liverpool.ac.uk>
2019-11-10 17:14:52 +0000

    Code is now validated using histograms of samples.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
-import pickle
+from numpy.random import multivariate_normal as mvn
 from matplotlib import pyplot as plt
-from scipy.stats import multivariate_normal as Normal_PDF
 from sklearn.mixture import GaussianMixture as GMM_sk
 from GMM_Conditional import *
 
@@ -16,47 +15,37 @@ To-do list is written in the GMM_Conditional.py class.
 P.L.Green
 """
 
-# Load 2D data from a mixture of 2 Gaussians
-file = open('2_cluster_data_unsupervised.dat', 'rb')
-X, N = pickle.load(file)
-n_components = 2
-file.close()
+# Generate samples from a Gaussian Mixture Model
+n_components=2
+X1 = mvn(mean=[3, 3], cov=np.eye(2), size=10000)
+X2 = mvn(mean=[-4, 0], cov=np.array([[1, 0.8],[0.8, 1]]), size=10000)
+X = np.vstack([X1, X2])
 
 # Fit a Gaussian mixture model
-gmm = GMM_sk(n_components)
+gmm = GMM_sk(n_components=n_components)
 gmm.fit(X)
 
-# Initialise plots
+# Our conditional value of x2
+x2 = 1.0
+
+# Histogram approximation of conditional distribution
+indices = np.where((X[:, 1] > x2 - 0.1) &
+                   (X[:, 1] < x2 + 0.1))
+
+# Create historgram
 fig, ax = plt.subplots(nrows=2, ncols=1)
-
-# Plot data
-ax[0].plot(X[:, 0], X[:, 1], 'k o', alpha=0.3)
-
-# Plot contours of GMM
-r1 = np.linspace(-7, 7, 100)
-r2 = np.linspace(-7, 7, 100)
-x_r1, x_r2 = np.meshgrid(r1, r2)
-pos = np.empty(x_r1.shape + (2, ))
-pos[:, :, 0] = x_r1
-pos[:, :, 1] = x_r2
-for c in range(n_components):
-    p = Normal_PDF(gmm.means_[c], gmm.covariances_[c])
-    ax[0].contour(x_r1, x_r2, gmm.weights_[c] * p.pdf(pos))
+ax[0].plot(X[:, 0], X[:, 1], 'k x')
+ax[0].plot(X[indices, 0], X[indices, 1], 'r o')
+ax[0].set_xlim([-8, 8])
+ax[1].hist(X[indices, 0].T, density=True, bins=50)
+ax[1].set_xlim([-8, 8])
 
 # Create our GMM_Conditional object
-x2 = 1      # Our conditional value of x2
 gmm_cond = GMM_Conditional(means=gmm.means_,
                            covariances=gmm.covariances_,
                            weights=gmm.weights_,
                            n_components=gmm.n_components,
                            D1=1, D2=1)
-
-# Plot line showing conditional value of x2
-ax[0].plot([-7, 7], np.repeat(x2, 2), 'r')
-ax[0].set_xlim([-7, 7])
-ax[0].set_ylim([-7, 7])
-ax[0].set_xlabel('$x_1$')
-ax[0].set_ylabel('$x_2$')
 
 # Plot conditional distribution over range of x1 values
 x1_range = np.linspace(-7, 7, 1000)
@@ -64,7 +53,7 @@ pdf = np.zeros(1000)
 for i in range(1000):
     pdf[i] = gmm_cond.pdf_x1_cond_x2(x1_range[i], x2)
 ax[1].plot(x1_range, pdf, 'k')
-ax[1].set_xlim([-7, 7])
+ax[1].set_xlim([-8, 8])
 ax[1].set_xlabel('x1')
 ax[1].set_ylabel('p(x1 | x2)')
 
